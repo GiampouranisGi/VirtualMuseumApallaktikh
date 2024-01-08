4d57ea05aa416d5e932d96dcbf61883631a045c8
asus <asus@LAPTOP-SMEG92DI>
2024-01-08 21:04:47 +0200

    Add project files.

---
+++ VirtualMuseum/Form1.Designer.cs
@@ -0,0 +1,68 @@
+﻿namespace VirtualMuseum
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.EnterButton = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // EnterButton
+            // 
+            this.EnterButton.AutoSize = true;
+            this.EnterButton.BackColor = System.Drawing.Color.Transparent;
+            this.EnterButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(161)));
+            this.EnterButton.ForeColor = System.Drawing.Color.Black;
+            this.EnterButton.Location = new System.Drawing.Point(401, 399);
+            this.EnterButton.Name = "EnterButton";
+            this.EnterButton.Size = new System.Drawing.Size(134, 39);
+            this.EnterButton.TabIndex = 0;
+            this.EnterButton.Text = "ENTER";
+            this.EnterButton.Click += new System.EventHandler(this.EnterButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::VirtualMuseum.Properties.Resources.ce671b_f329038675fa47a9a41853f5d82bebd0_mv2;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(943, 568);
+            this.Controls.Add(this.EnterButton);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label EnterButton;
+    }
+}
+
